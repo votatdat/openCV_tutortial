963ce6445ba81a3d563e58d2c4381cfe273201f3
Vo Tat Dat <tatdat13@gmail.com>
2020-11-09 13:45:41 +0700

    Finished 'Reading Images & Video' part

---
+++ read.py
@@ -0,0 +1,17 @@
+import cv2 as cv
+
+# img = cv.imread('Photos/cat_large.jpg')
+# cv.imshow('Cat', img)
+# cv.waitKey(0)
+
+capture = cv.VideoCapture('Videos/dog.mp4')
+
+while True:
+    isTrue, frame = capture.read()
+    cv.imshow('Video', frame)
+
+    if cv.waitKey(20) & 0xFF == ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
